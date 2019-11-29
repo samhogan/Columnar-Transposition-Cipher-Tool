3dfd3b3c95e2b31b952ae2d0fc00d7cdab9c8f55
samhogan <sam@samhogan.net>
2019-11-29 16:12:15 -0500

    can decipher ciphertext

--- index.js
+++ index.js
@@ -24,10 +24,33 @@ function cleanText(text)
     return text.replace(/[^A-Za-z]/g,"").toLowerCase();
 }
 
-function cleanKeyword()
+
+var keywordOrder = [];
+var keywordLocs = []
+function setUpKeyword()
 {
     keyword = cleanText(keywordInput.value);
     keywordInput.value = keyword;
+
+    //entries hold the array locations of the index (backward)
+    keywordLocs = [];
+    for(var i=0; i<keyword.length; i++)
+    {
+        keywordLocs.push(i);
+    }
+
+    keywordLocs.sort(function(a,b){
+        return keyword.charCodeAt(a) - keyword.charCodeAt(b);
+    });
+
+    //now swap the index with the value
+    keywordOrder = [];
+    for(var i=0; i<keyword.length; i++)
+    {
+        keywordOrder[keywordLocs[i]] = i;
+    }
+
+
 }
 
 function cleanPadding()
@@ -41,6 +64,52 @@ function decipher()
 {
     console.log("deciphering...");
     
+    var ciphertext = cleanText(ciphertextArea.value);
+    setUpKeyword();
+    cleanPadding();
+
+    //determine number of rows and columns
+    var cols = keyword.length;
+    var rows = Math.ceil(ciphertext.length / cols);
+
+    //the rectangle
+    textArray = [];
+
+    //build the text array (the rectangle)
+    for(var c=0; c<cols; c++)
+    {
+        textArray.push([]);
+    }
+
+    var i=0;
+    for(var c=0; c<cols; c++)
+    {
+        for(var r=0; r<rows; r++)
+        {
+            textArray[c].push(ciphertext.charAt(i));
+            i++;
+        }
+    }
+
+    //sort it to be in the order of the keyword
+    textArray.sort(function(a, b){  
+        return keywordLocs[textArray.indexOf(a)] - keywordLocs[textArray.indexOf(b)];
+    });
+
+    console.log(textArray);
+
+    //build the plaintext
+    var plaintext = "";
+    for(var r=0; r<rows; r++)
+    {
+        for(var c=0; c<cols; c++)
+        {
+            plaintext += textArray[c][r];
+        }
+    }
+
+
+    plaintextArea.value = plaintext;
 }
 
 
@@ -49,7 +118,8 @@ function encipher()
     console.log("enciphering...");
     
     var plaintext = cleanText(plaintextArea.value);
-    cleanKeyword();
+    setUpKeyword();
+    console.log(keywordOrder);
     cleanPadding();
     
     //determine number of rows and columns
@@ -58,6 +128,7 @@ function encipher()
 
     var paddingCount = rows*cols - plaintext.length;
 
+    //add padding
     for(var i=0; i<paddingCount; i++)
     {
         plaintext+=padding;
@@ -65,6 +136,7 @@ function encipher()
 
     textArray = [];
 
+    //build the text array (the rectangle)
     for(var c=0; c<cols; c++)
     {
         textArray.push([]);
@@ -80,16 +152,36 @@ function encipher()
         }
     }
 
-    textArray.sort(function(a, b){  
-        return keyword.charAt(a) - keyword.charAt(b);
+    //sort the columns by the order they are taken out "out by columns"
+    var textArrayColsInOrder = textArray.slice(0);
+
+    textArrayColsInOrder.sort(function(a, b){  
+        //console.log(keyword.charCodeAt(textArray.indexOf(a)) - keyword.charCodeAt(textArray.indexOf(b)));
+        return keyword.charCodeAt(textArray.indexOf(a)) - keyword.charCodeAt(textArray.indexOf(b));
     });
 
-    console.log(textArray);
+   // console.log(textArray);
+   // console.log(textArrayColsInOrder);
+
+   //build the ciphertext
+    var cipherText = "";
+    var charsSinceSpace = 0;
+    for(var c=0; c<cols; c++)
+    {
+        for(var r=0; r<rows; r++)
+        {
+            cipherText += textArrayColsInOrder[c][r];
+            charsSinceSpace++;
+            if(charsSinceSpace == 5)
+            {
+                cipherText += " ";
+                charsSinceSpace = 0;
+            }
+        }
+    }
 
 
-    ciphertextArea.value = plaintext;
-
-
+    ciphertextArea.value = cipherText.toUpperCase();
 
 
 
